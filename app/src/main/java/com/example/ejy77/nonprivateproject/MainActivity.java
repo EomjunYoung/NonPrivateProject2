ebd9130ab021b67070a746204e6c7671184ff6f3
EomjunYoung <theoneday737@naver.com>
2017-09-24 19:52:08 +0900

    Notification Ex 1

--- app/src/main/java/com/example/ejy77/nonprivateproject/MainActivity.java
+++ app/src/main/java/com/example/ejy77/nonprivateproject/MainActivity.java
@@ -1,45 +1,51 @@
 package com.example.ejy77.nonprivateproject;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
 
-    private Button btn1, btn2;
+public class MainActivity extends AppCompatActivity
+
+{
+
+
+    Button btn1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
 
         btn1 = (Button)findViewById(R.id.btn1);
-        btn2 = (Button)findViewById(R.id.btn2);
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(getApplicationContext(), "Service 시작", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, MyService.class);
-                startService(intent);
+                NotificationManager notificationManager = (NotificationManager)MainActivity.this.getSystemService(MainActivity.this.NOTIFICATION_SERVICE);
+                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
 
-            }
-        });
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext());
+                intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                builder.setTicker("??");
+                builder.setContentTitle("뭘까?");
+                builder.setContentText("대체 뭘까!!!?");
+                builder.setDefaults(Notification.DEFAULT_SOUND);
+                builder.setDefaults(Notification.DEFAULT_VIBRATE);
 
-
-        btn2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Toast.makeText(getApplicationContext(), "Service 종료", Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this, MyService.class);
-                stopService(intent);
-
+                PendingIntent pendingIntent = PendingIntent.getActivity(MainActivity.this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                builder.setContentIntent(pendingIntent);
+                builder.setAutoCancel(true);
+                notificationManager.notify(1, builder.build());
 
             }
         });
---
+++ app/src/main/java/com/example/ejy77/nonprivateproject/MainActivity2.java
@@ -0,0 +1,8 @@
+package com.example.ejy77.nonprivateproject;
+
+/**
+ * Created by ejy77 on 2017-09-24.
+ */
+
+public class MainActivity2 {
+}
