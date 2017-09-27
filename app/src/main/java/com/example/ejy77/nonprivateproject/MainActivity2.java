1304f2ade564a9ad18689c5872bb9f14d10b529d
EomjunYoung <theoneday737@naver.com>
2017-09-27 19:34:52 +0900

    Notification Ex 3

--- app/src/main/java/com/example/ejy77/nonprivateproject/MainActivity2.java
+++ app/src/main/java/com/example/ejy77/nonprivateproject/MainActivity2.java
@@ -1,8 +1,26 @@
 package com.example.ejy77.nonprivateproject;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+import android.widget.Toast;
+
 /**
  * Created by ejy77 on 2017-09-24.
  */
 
-public class MainActivity2 {
+public class MainActivity2 extends AppCompatActivity{
+
+    TextView tv1;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+        tv1 = (TextView)findViewById(R.id.tv1);
+        Toast.makeText(getApplicationContext(), "WHY", Toast.LENGTH_SHORT).show();
+    }
 }
